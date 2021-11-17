890e93a50ddba229d2a91dba9b6d5d08f0e750e7
SieR-VR <nwh63@naver.com>
2021-11-17 14:12:11 +0900

    [Front, Back] Delete SSR

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import Next from 'next';
-import { RenderModule } from 'nest-next';
-
 import { PlayersModule } from "./players/players.module";
 import { Player } from "./players/models/player.entity";
-
-import { AppController } from './app.controller';
 
 @Module({
   imports: [
     PlayersModule,
-    RenderModule.forRootAsync(
-      Next({ 
-        dev: process.env.NODE_ENV !== 'production',
-        conf: { useFileSystemPublicRoutes: false },
-      }),
-    ),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db',
@@ -25,7 +14,7 @@ import { AppController } from './app.controller';
       synchronize: true,
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
 })
 
 export class AppModule { }
--- src/players/services/players.service.ts
+++ src/players/services/players.service.ts
@@ -7,25 +7,25 @@ import { Player } from "../models/player.entity";
 @Injectable()
 export class PlayersService {
   constructor(@InjectRepository(Player) 
-    private playerRepository: Repository<Player>) {}
+    private PlayerRepository: Repository<Player>) {}
 
   async findAll(): Promise<Player[]> {
-    return await this.playerRepository.find();
+    return await this.PlayerRepository.find();
   }
 
   async findOne(id: number): Promise<Player | undefined> {
-    return await this.playerRepository.findOne(id);
+    return await this.PlayerRepository.findOne(id);
   }
 
   async create(player: Player): Promise<Player> {
-    return await this.playerRepository.save(player);
+    return await this.PlayerRepository.save(player);
   }
 
   async update(id: number, player: Player): Promise<UpdateResult> {
-    return await this.playerRepository.update(id, player);
+    return await this.PlayerRepository.update(id, player);
   }
 
   async delete(id: number): Promise<DeleteResult> {
-    return await this.playerRepository.delete(id);
+    return await this.PlayerRepository.delete(id);
   }
 }
