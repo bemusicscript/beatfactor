4e68beb9ae5b33ec35d453b6923c0bb53f595d51
SieR-VR <nwh63@naver.com>
2021-11-18 23:39:07 +0900

    Use HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Header, Footer } from "./Essentials";
 import { Home, Ranking } from "./Pages";
